e7a1818cf3206577f1fa46a20226883b55a82e0a
David(gecert) <dkarandze4@gmail.com>
2022-08-01 11:17:02 +0200

    Created models, views, migrations, add debug_tool_bar

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,45 @@
-from django.contrib import admin  # noqa: F401
+from django.contrib import admin
 
-# Register your models here.
+from .models import Author, Book, Publisher, Store
+
+
+class BookInlineModelAdmin(admin.TabularInline):
+    model = Book
+
+
+class PublisherInlineModelAdmin(admin.TabularInline):
+    model = Publisher
+
+
+@admin.register(Author)
+class AuthorModelAdmin(admin.ModelAdmin):
+    list_display = ["name", "surname", "country"]
+    fields = ['name', 'surname', "country"]
+    search_fields = ["surname"]
+    inlines = [BookInlineModelAdmin]
+
+
+@admin.register(Book)
+class BookModelAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'author']
+    fields = ['title', 'pubdate', 'author', 'publisher', ('price', 'pages')]
+    raw_id_fields = ['author', ]
+    date_hierarchy = "pubdate"
+    list_filter = ['price', 'pubdate']
+    filter_vertical = ["publisher"]
+    search_fields = ["author", "title"]
+
+
+@admin.register(Store)
+class StoreModelAdmin(admin.ModelAdmin):
+    list_display = ["name", "address"]
+    fields = ['name', 'address']
+    search_fields = ["name", "address"]
+    inlines = [PublisherInlineModelAdmin]
+
+
+@admin.register(Publisher)
+class PublisherModelAdmin(admin.ModelAdmin):
+    list_display = ["name", "store"]
+    fields = ['name', "year", 'store']
+    search_fields = ["name", "store"]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,139 @@
-from django.shortcuts import render  # noqa: F401
+from catalog.models import Author, Book, Publisher, Store
 
-# Create your views here.
+from django.db.models import Avg, Count, Max, Min
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'catalog/index.html')
+
+
+def books_list(request):
+    info = Book.objects.all().aggregate(Avg('price'),
+                                        Max('price'),
+                                        Min('price'),
+                                        Count('title'),
+                                        Max('pages'),
+                                        Min('pages')
+                                        )
+    book_list = Book.objects.select_related('author').all()
+    books = []
+    for book in book_list:
+        books.append({'id': book.id,
+                      'title': book.title,
+                      'author': book.author.surname
+                      }
+                     )
+    return render(request, 'catalog/books.html',
+                  {'books': books,
+                   'info': info}
+                  )
+
+
+def book_info(request, id):
+    book = Book.objects.select_related('author').get(id=id)
+    publisher = Publisher.objects.prefetch_related('book_set__publisher').filter(book=id)
+    return render(
+        request,
+        'catalog/book_info.html',
+        {'id': book.id,
+         'title': book.title,
+         'pages': book.pages,
+         'price': book.price,
+         'pubdate': book.pubdate,
+         'author': book.author.surname,
+         'author_id': book.author.id,
+         'publisher': publisher,
+         'publisher_id': book.publisher.name
+         }
+    )
+
+
+def authors_list(request):
+    authors = Author.objects.all().annotate(count=Count('surname'))
+    return render(request, 'catalog/authors.html',
+                  {'authors': authors,
+                   }
+                  )
+
+
+def author_info(request, id):
+    author = Author.objects.get(id=id)
+    book_author = Book.objects.select_related('author').filter(author_id=id)
+    return render(
+        request,
+        'catalog/author_info.html',
+        {'id': author.id,
+         'name': author.name,
+         'surname': author.surname,
+         'country': author.country,
+         'books': book_author
+         }
+    )
+
+
+def stores_list(request):
+    store_list = Store.objects.select_related('publisher').all()
+    stores = []
+    for store in store_list:
+        count = Book.objects.prefetch_related('publisher__store').filter(publisher__name=store.publisher.name). \
+            annotate(count=Count('title'))
+
+        stores.append({'id': store.id,
+                       'name': store.name,
+                       'address': store.address,
+                       'publisher': store.publisher.name,
+                       'count': count
+                       }
+                      )
+    return render(request, 'catalog/stores.html',
+                  {'stores': stores,
+                   }
+                  )
+
+
+def stores_info(request, id):
+    store = Store.objects.prefetch_related('publisher').get(id=id)
+    books = Book.objects.prefetch_related('publisher').filter(publisher__name=store.publisher.name)
+    return render(request,
+                  'catalog/stores_info.html',
+                  {'id': store.id,
+                   'name': store.name,
+                   'address': store.address,
+                   'publisher': store.publisher.name,
+                   'books': books
+                   }
+                  )
+
+
+def publishers_list(request):
+    publisher_list = Publisher.objects.select_related('store').all()
+    pub_list = []
+    for publisher in publisher_list:
+        books = Book.objects.prefetch_related('publisher').filter(publisher__name=publisher.name).\
+            aggregate(Avg('price'))
+        pub_list.append(
+            {'name': publisher.name,
+             'store': publisher.store.name,
+             'pk': publisher.pk,
+             'books': books
+             }
+        )
+    return render(request, 'catalog/publishers.html',
+                  {'pub_list': pub_list,
+                   }
+                  )
+
+
+def publisher_info(request, pk):
+    publisher = Publisher.objects.get(pk=pk)
+    books = Book.objects.prefetch_related('publisher').filter(publisher__name=publisher.name)
+    return render(request,
+                  'catalog/publisher_info.html',
+                  {'id': publisher.pk,
+                   'name': publisher.name,
+                   'store': publisher.store,
+                   'year': publisher.year,
+                   'books': books
+                   }
+                  )
